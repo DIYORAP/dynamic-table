e56c8a4015328ecab9527894134652ef14190695
Parthik Diyora <ddiyora503@gmail.com>
2025-05-02 23:14:28 +0530

    solve

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})l
+})
 
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }));
 app.use(express.json());
