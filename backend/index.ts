a4bb4be1e666eb6cec2f47b9bc7a099386c0ceb7
Parthik Diyora <ddiyora503@gmail.com>
2025-05-02 23:14:11 +0530

    express

--- backend/index.ts
+++ backend/index.ts
@@ -8,6 +8,11 @@ dotenv.config();
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})l
+
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }));
 app.use(express.json());
 app.use('/api', recordRoutes);
